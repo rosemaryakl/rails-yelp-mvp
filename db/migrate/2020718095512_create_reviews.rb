b042a1111d7b4a2bcef51559704ea33b8ce4d8c7
Rosemary Akl <aklrosemary1@gmail.com>
2020-07-18 16:19:41 +0100

    yelp

--- db/migrate/2020718095512_create_reviews.rb
+++ db/migrate/2020718095512_create_reviews.rb
@@ -1,4 +1,4 @@
-class CreateReviews < ActiveRecord::Migration[5.2]
+class CreateReviews < ActiveRecord::Migration[6.2]
   def change
     create_table :reviews do |t|
       t.text :content
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020718095512) do
+ActiveRecord::Schema.define(version: 2020_07_18_110813) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020718095512) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "chef_name"
   end
 
   create_table "reviews", force: :cascade do |t|
