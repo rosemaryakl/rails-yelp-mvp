c8929d2e269e2e9e077de7904b6da465485fc590
Rosemary Akl <aklrosemary1@gmail.com>
2020-07-23 20:36:08 +0100

    rails new

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: (0..5).to_a }, numericality: { only_integer: true }
-
+  validates :rating, presence: true, inclusion: { in: (0..5)}, numericality: { only_integer: true }
 end
